54ab12b451a546311a2b3ba8f789cb3c8bc3008c
maruzsanborisz05-rgb <maruzsanborisz05@gmail.com>
2026-05-23 23:21:07 +0200

    Grafikus felulet: Fazis 4-5 (Controller, Integracio)

--- 09-proto/proto/src/Main.java
+++ 09-proto/proto/src/Main.java
@@ -2,14 +2,31 @@ import javax.swing.*;
 
 /**
  * A grafikus alkalmazas belepesi pontja.
- * Letrehozza a minta-palyat es megjeleníti a jatekot.
+ * Letrehozza a minta-palyat es megjeleniti a jatekot.
+ * A regi Proto is elerheto marad: java -cp out Proto
  */
 public class Main {
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
+            try {
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            } catch (Exception ignored) {}
+
             Game game = MapBuilder.buildSampleGame();
-            new GameFrame(game);
+            GameFrame frame = new GameFrame(game);
+
+            JOptionPane.showMessageDialog(frame,
+                "Udvozollek a Banducole jatekban!\n\n"
+                + "Takarito jatekos:\n"
+                + "  - Kattints egy hokotrodra a kijelolesehez\n"
+                + "  - Kattints egy szomszedos savra a lepeshez es takaritashoz\n"
+                + "  - A jobb oldali panelen vasarolhatsz fejeket es uzemanyagot\n\n"
+                + "Buszsofor jatekos:\n"
+                + "  - Kattints egy szomszedos savra a busz leptesehez\n\n"
+                + "A 'Kovetkezo jatekos >>' gombbal adhatod at a kort.",
+                "Jatekszabalyok",
+                JOptionPane.INFORMATION_MESSAGE);
         });
     }
 }
