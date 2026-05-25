3511cf8fd1e400271595c6e459b1d050359521ad
Adoskex <211139775+Adoskex@users.noreply.github.com>
2026-05-25 13:58:49 +0200

    Gyökérből törölve a duplikált fájlok

--- IDrawable.java
+++
@@ -1,9 +0,0 @@
-import java.awt.Graphics2D;
-
-/**
- * Grafikus elemek kozos interfesze.
- * Minden megjelenitheto elem (sav, csomopont, jarmu, hokotro) implementalja.
- */
-public interface IDrawable {
-    void draw(Graphics2D g);
-}
